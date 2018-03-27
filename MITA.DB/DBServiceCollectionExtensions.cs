1d923a21423ae29b04f9ae5b4b6c98a474367808
taihon <taihon@users.noreply.github.com>
2018-03-27 14:20:34 +0700

    add entities and context

---
+++ MITA.DB/DBServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MITA.DB
+{
+    public static class DBServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTasksDB(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TasksContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}

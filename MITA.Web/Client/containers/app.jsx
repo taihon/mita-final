7d03d16f43df856b24ed1bb266cd79d3f63a6929
taihon <taihon@users.noreply.github.com>
2018-03-29 14:45:39 +0700

    add minimal react app

---
+++ MITA.Web/Client/containers/app.jsx
@@ -0,0 +1,3 @@
+﻿import React, { Component } from 'react';
+
+export const App = (props) => (<div> Hello React App!</div>);
---
+++ MITA.Web/Client/index.jsx
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './containers/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById("content"));

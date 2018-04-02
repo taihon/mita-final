7936e468a56a3b778763cc83e243191de0dbdede
taihon <taihon@users.noreply.github.com>
2018-04-02 12:53:21 +0700

    change archive project to be projects subcomponent

--- MITA.Web/Client/pages/ArchivedProjects.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Page } from '../components/page/Page';
-export const ArchivedProjects = (props) => (
-    <Page>
-        <h3>Archived projects page</h3>
-    </Page>
-)

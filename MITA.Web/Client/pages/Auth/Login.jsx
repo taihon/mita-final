d6382799a4c20e1afaaa272cc126140ed3368d40
taihon <taihon@users.noreply.github.com>
2018-04-03 13:40:33 +0700

    move login to Auth folder
    this should keep all auth-related components together

--- MITA.Web/Client/pages/Auth/Login.jsx
+++ MITA.Web/Client/pages/Auth/Login.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 
-import { Page } from '../components/page/Page';
-import Input from '../components/input/Input';
-import * as actions from '../store/actions';
+import { Page } from '../../components/page/Page';
+import Input from '../../components/input/Input';
+import * as actions from '../../store/actions';
 
 class Login extends Component {
     state = {
         login: "",
-        password: ""
+        password: "",
     }
     inputChangedHandler = (event, control) => {
         this.setState({
             ...this.state,
-            [control]: event.target.value
-        })
+            [control]: event.target.value,
+        });
     }
     submitHandler = (event) => {
         event.preventDefault();
@@ -29,13 +28,15 @@ class Login extends Component {
                         placeholder="Login"
                         id="login"
                         value={this.state.login}
-                        onChange={(event) => this.inputChangedHandler(event, "login")} />
+                        onChange={event => this.inputChangedHandler(event, "login")}
+                    />
                     <Input
                         placeholder="password"
                         type="password"
                         id="password"
                         value={this.state.password}
-                        onChange={(event) => this.inputChangedHandler(event, "password")} />
+                        onChange={event => this.inputChangedHandler(event, "password")}
+                    />
                     <button type="submit">Login</button><br />
                     <button>Login external</button>
                 </form>
@@ -43,15 +44,16 @@ class Login extends Component {
         );
     }
 }
-const mapStateToProps = state => {
-    return {
+const mapStateToProps = state => (
+    {
         user: state.user,
-        token: state.token
+        token: state.token,
     }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        onAuth: (email, password) => dispatch(actions.login(email, password))
+);
+const mapDispatchToProps = dispatch => (
+    {
+        onAuth: (email, password) =>
+            dispatch(actions.login(email, password)),
     }
-}
+);
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
